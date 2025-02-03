fdea514f20cc006f85ce21efdd369aef6a8d164f
FDfatimadaruich <fatimadaruich.fd@gmail.com>
2025-02-03 13:07:00 -0300

    review2

---
+++ my-app/src/app/registersale/page.tsx
@@ -0,0 +1,11 @@
+import FormRegisterSale from "@/components/formRegisterSale/FormRegisterSale";
+
+export default function page() {
+    return (
+      <div className="text-black">
+     <FormRegisterSale/>
+      </div>
+      
+    );
+  }
+  
---
+++ my-app/src/app/review2/page.tsx
@@ -0,0 +1,10 @@
+import FormReseña from "@/components/formReseña/FormReseña";
+
+export default function page() {
+    return (
+      <div className="text-black">
+     <FormReseña/>
+      </div>
+      
+    );
+  }
